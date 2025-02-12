54f76934510a3d8e83074b03b2fa7ef186fc05c0
KAnirban8 <kazianirban08@gmail.com>
2025-02-12 20:03:46 +0530

    search navigate

--- src/app/shared/components/search-bar/search-bar.component.ts
+++ src/app/shared/components/search-bar/search-bar.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-search-bar',
@@ -8,9 +9,23 @@ import { Component } from '@angular/core';
   styleUrl: './search-bar.component.scss'
 })
 export class SearchBarComponent {
+  @Input() searchNavigate !: boolean;
+  @Output() customEvent = new EventEmitter<boolean>();
+
   locations = [
     { value: 'bd', label: 'Location BD' },
     { value: 'us', label: 'Location US' },
     { value: 'uk', label: 'Location UK' },
   ];
+
+  constructor(private router : Router){
+    console.log(this.searchNavigate);
+    
+  }
+
+  search(){
+    this.customEvent.emit(true);
+  }
+
+
 }
